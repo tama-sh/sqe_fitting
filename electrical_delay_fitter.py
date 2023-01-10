85306737378bd45ca6cce5ac961f17adc50d2c13
unknown <tama.csh@gmail.com>
2023-01-10 20:18:31 +0900

    Changed utility function, fixed bug in electrical delay correction

--- electrical_delay_fitter.py
+++ electrical_delay_fitter.py
@@ -3,25 +3,28 @@ from typing import Tuple
 import numpy as np
 import lmfit
 from .util import percentile_range_data
-from .signal_util import group_delay, smoothing
+from .signal_util import group_delay, smoothen
 from .circle_fitter import algebric_circle_fit
 
-def correct_electrical_delay(omega: np.ndarray, cplx: np.ndarray, electrical_delay=None, correct_phase_offset=True):
+def correct_electrical_delay(omega: np.ndarray, cplx: np.ndarray, electrical_delay=None, phase_offset=None, phase_auto_correct=False):
     """Correct electrical delay of complex data
     
     Args:
         omega (np.ndarray): angular frequency
         cplx (np.ndarray): complex data with electrical delay
-        electrical_delay (bool): the value of electrical_delay. if None, automatically estimate the electrical delay. default is None
-        correct_phase_offset (bool): if True, automatically correct phase offset as well. default is True
+        electrical_delay (bool): the value of electrical_delay. if None, automatically estimate the electrical delay from group delay. default is None
+        phase_offset (float): phase offset to be corrected. if not None, the phase offset is corrected with this value. default is None.
+        phase_auto_correct (bool): if True, automatically correct phase offset as well. if phase_offset is given, this parameter is not active. default is False
     
     Returns:
         np.ndarray: complex data with electrical delay corrected
     """
     if electrical_delay is None:
-        electrical_delay = estimate_electrical_delay(omega, cplx)
+        electrical_delay = estimate_electrical_delay_from_group_delay(omega, cplx)
     cplx_c = cplx*np.exp(1j*electrical_delay*omega)
-    if correct_phase_offset:
+    if phase_offset:
+        return cplx_c*np.exp(-1j*phase_offset)
+    elif phase_auto_correct:
         return cplx_c*np.exp(-1j*np.angle(cplx_c[0]+cplx_c[-1]))
     else:
         return cplx_c
@@ -54,7 +57,7 @@ def estimate_electrical_delay_from_group_delay(omega: np.ndarray, cplx: np.ndarr
         float: electrical delay
     """
     if with_smoothing:
-        cplx = smoothing(cplx)
+        cplx = smoothen(cplx)
     delay = group_delay(omega, cplx)
     return np.mean(percentile_range_data(delay, *percentile_range))
 
@@ -84,7 +87,7 @@ def estimate_electrical_delay_circle_fit(omega: np.ndarray, cplx: np.ndarray, el
         parvals = pars.valuesdict()
         electrical_delay = parvals['electrical_delay']
         
-        cplx_c = cplx*np.exp(-1j*omega*electrical_delay)
+        cplx_c = cplx*np.exp(1j*omega*electrical_delay)
         x = cplx_c.real
         y = cplx_c.imag
         rst = algebric_circle_fit(x, y)
@@ -101,8 +104,8 @@ def estimate_electrical_delay_circle_fit(omega: np.ndarray, cplx: np.ndarray, el
     else: 
         return rst.params['electrical_delay'].value
     
-def estimate_electrical_delay(omega: np.ndarray, cplx: np.ndarray):
-    """Estimate electridal delay
+def estimate_electrical_delay_resonator(omega: np.ndarray, cplx: np.ndarray):
+    """Estimate electridal delay for resonator data
     
     Use estimate_electrical_delay_from_group_delay to give an initial guess and use estimate_electrical_delay_circle_fit after that
     
--- signal_util.py
+++ signal_util.py
@@ -1,8 +1,8 @@
 import numpy as np
 import scipy.signal as scisig
 
-def smoothing(data: np.ndarray, dt: float = 1, numtaps: int = 21, smoothing_width: float = 10):
-    """Smooth the data by applying fir filter with zero phase.
+def smoothen(data: np.ndarray, dt: float = 1, numtaps: int = 21, smoothing_width: float = 10):
+    """Smoothen the data by applying fir filter with zero phase.
     Args:
         data (np.ndarray): data to be smoothened
         dt (float): interval of sampling
@@ -17,6 +17,31 @@ def smoothing(data: np.ndarray, dt: float = 1, numtaps: int = 21, smoothing_widt
     data_lp = scisig.filtfilt(b, 1, data) # apply zero phase filter
     return data_lp
 
+def derivative(y: np.ndarray, x: np.ndarray):
+    """Take numerical derivative
+    
+    Args:
+        y (np.ndarray): dependent variable
+        x (np.ndarray): independent variable
+        
+    Returns:
+        np.ndarray: array of derivative at (x[1:]+x[:-1])/2 ,the length of array become len(x)-1
+    """
+    return (y[1:]-y[:-1])/(x[1:]-x[:-1])
+
+def middle_points(x: np.ndarray):
+    """Return the middle points of array
+    
+    Return the middle point of adjacent coordinate (x_i + x_{i+1})/2 as an array
+    
+    Args:
+        x (np.ndarray): array of coordinate
+    
+    Return:
+        np.ndarray: array of middle point
+    """
+    return 0.5*(x[1:]+x[:-1])
+
 def group_delay(omega: np.ndarray, cplx: np.ndarray):
     """Calculate group delay
     
@@ -27,6 +52,6 @@ def group_delay(omega: np.ndarray, cplx: np.ndarray):
         cplx (np.ndarray): complex data with electrical delay
         
     Returns:
-        np.ndarray: group delay at (omega[1:]-omega[:-1])/2 ,the length of array become len(cplx)-1
+        np.ndarray: group delay at the middle points of give array of omega ,the length of array become len(cplx)-1
     """
-    return -np.imag((cplx[1:]-cplx[:-1])/(0.5*(cplx[1:]+cplx[:-1])))/(omega[1:]-omega[:-1])
+    return -np.imag(derivative(cplx, omega)/middle_points(cplx))
