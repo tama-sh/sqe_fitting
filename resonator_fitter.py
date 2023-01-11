b55072cfb0b08e1e0e1697c719b23fb323bf0322
unknown <tama.csh@gmail.com>
2023-01-11 18:08:12 +0900

    Fixed a small bug of resonator fitter

--- resonator_fitter.py
+++ resonator_fitter.py
@@ -4,11 +4,11 @@ class resonator_fitter(object):
     def __init__(self, reflection_type='normal'):
         self.model = ResonatorReflectionModel(reflection_type=reflection_type)
     
-    def guess(self, cplx, omega, fix_electrical_delay=False, **kwargs):
+    def guess(self, cplx, omega, fix_electrical_delay=True, **kwargs):
         pars = self.model.guess(cplx, omega=omega, fix_electrical_delay=fix_electrical_delay, **kwargs)
         return pars
     
-    def fit(self, cplx, omega, params=None, fix_electrical_delay=False, **kwargs):
+    def fit(self, cplx, omega, params=None, fix_electrical_delay=True, **kwargs):
         if params is None:
-            params = self.guess(cplx, omega, fix_electrical_delay=False)
+            params = self.guess(cplx, omega, fix_electrical_delay=fix_electrical_delay)
         return self.model.fit(cplx, params, omega=omega, **kwargs)
